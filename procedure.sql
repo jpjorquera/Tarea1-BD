7ac11a765fd5d55d80fc46ae390a27bdb27ffca7
Jorge Contreras <jorge.contrerasc@sansano.usm.cl>
2016-10-28 23:30:48 -0300

    procedure actualizado

--- procedure.sql
+++ procedure.sql
@@ -7,10 +7,13 @@ create or replace PROCEDURE procedure_maestro (par IN number)
       WHERE pokemon.ID_POKEMON = salvaje.POKEMON_ID AND
             (par in (SELECT usuario.ID_USUARIO FROM usuario)) AND
             (
-            (salvaje.ID_SALVAJE = pokedex.SALVAJE_ID AND pokedex.USUARIO_ID = par AND pokedex.ESTADO = 0) 
+            (salvaje.ID_SALVAJE = pokedex.SALVAJE_ID AND pokedex.USUARIO_ID = par AND pokedex.ESTADO = 0 AND
+             salvaje.POKEMON_ID NOT IN (SELECT salvaje.POKEMON_ID FROM pokedex,salvaje WHERE pokedex.USUARIO_ID = par AND
+                                                                                             pokedex.SALVAJE_ID = salvaje.ID_SALVAJE AND
+                                                                                             pokedex.ESTADO = 1)) 
             OR 
-            (salvaje.ID_SALVAJE NOT IN (SELECT pokedex.SALVAJE_ID FROM pokedex WHERE par = pokedex.USUARIO_ID))
-            )
+            (salvaje.ID_SALVAJE NOT IN (SELECT pokedex.SALVAJE_ID FROM pokedex WHERE par = pokedex.USUARIO_ID)
+            ))
       )
     LOOP
       dbms_output.put_line(rec.ID_POKEMON || ',' || rec.NOMBRE_POKEMON);
